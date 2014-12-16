2c7c410de2a3e648281f493d92c94272fb977018
Amy de Buitléir <amy@nualeargais.ie>
2014-12-16 12:49:46 +0000

    v4.1.1

--- src/ALife/Creatur/Wain/Checkpoint.hs
+++ src/ALife/Creatur/Wain/Checkpoint.hs
@@ -55,8 +55,8 @@ enforce
     => [Statistic] -> Checkpoint -> StateT u IO ()
 enforce xs c@(Check start key lim) = do
   t <- currentTime
-  writeToLog $ "DEBUG: checkpoint=" ++ show c ++ " t=" ++ show t
-    ++ " val=" ++ show (lookup key xs)
+  -- writeToLog $ "DEBUG: checkpoint=" ++ show c ++ " t=" ++ show t
+  --   ++ " val=" ++ show (lookup key xs)
   case lookup key xs of
     Just x -> when (t >= start && x `fails` lim) $ requestShutdown 
                ("failed check " ++ show c ++ " " ++ key ++ "=" ++ show x)
