54eaf0ef604670f87ae08c7e5f81f8ca7faaccc7
Amy de Buitléir <amy@nualeargais.ie>
2021-07-26 16:47:05 +0100

    refresh

--- default.nix
+++ default.nix
@@ -3,8 +3,8 @@ let
 in 
   pkgs.haskellPackages.developPackage {
     root = ./.;
-    modifier = drv:
-      pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages;
-        [ cabal-install
-        ]);
+    source-overrides = {
+      som = ../som;
+      creatur = ../creatur;
+    };
   }
